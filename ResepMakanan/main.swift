b53582ba596a3fbc578576209e9fbc107ae87643
irfan wahendra <2111527002_irfan@student.unand.ac.id>
2024-05-07 23:11:52 +0700

    first commit

--- ResepMakanan/main.swift
+++ ResepMakanan/main.swift
@@ -7,5 +7,52 @@
 
 import Foundation
 
-print("Hello, World!")
+print("Dicoding, Resto")
+print("Mari membuat makanan")
 
+// Mark: Memasukkan resep dan jumlah makanan.
+print("________________________")
+print("Apa yang ingin anda buat?")
+let food = readLine()
+
+print("Masukkan jumlah item yang ingin Anda masukkan", terminator: ":")
+let newITem = readLine() ?? "0"
+
+print("________________________")
+
+// Mark: Mengkonversi jumlah item yang perlu dimasukkan.
+if let totalItem: Int = Int(newITem){
+    var recipe = [String]()
+    
+    for index in 1...totalItem {
+        print("Masukkan item ke \(index)", terminator: ":")
+        let newItem = String(readLine() ?? "")
+        recipe.append(newItem)
+    }
+    
+    // Mark: Memasukkan waktu yang dibutuhkan untuk membuat resep.
+    print("________________________")
+    print("Berapa lama proses memasak dilakukan?")
+    let time = readLine() ?? ""
+    print("________________________")
+
+    //Mark: Mengurutkan Item Resep.
+    recipe.sort()
+    
+    //Mark: Menampilkan Hasil Resep.
+    print("Anda akan membuat \(String(describing: food)) selama \(time) dengan resep:")
+    for (index, value) in recipe.enumerated() {
+        print(" \(index+1). \(value)")
+    }
+} else {
+    print("Input tidak valid")
+}
+
+print("________________________")
+
+
+
+
+
+
+    
